ce2fdb7ce84cb5ed247cfca2897f9187d37ae3ac
uskatebolt <54988275+uskatebolt@users.noreply.github.com>
2020-04-30 16:16:42 -0400

    Add files via upload

---
+++ map-services.js
@@ -0,0 +1,12 @@
+export const mapServices = [
+  {
+    name: 'OpenStreetMap',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  },
+  {
+    name: 'Mapbox',
+    attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
+    url: "https://api.mapbox://styles/chefcurry/ck9myfesf043r1inral6gw1s2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2hlZmN1cnJ5IiwiYSI6ImNrOW13d3M1YTAwNHczam91ejNyN3Fkc2UifQ.yfglAK30_NoMxE7MtFPzwQ",
+  }
+];
